b3c7f5e0e1579cb22fd1391cde28daecf9abc73b
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-21 01:34:51 +0700

    bit view

---
+++ app/src/main/java/uit/tool/app/Main.java
@@ -0,0 +1,34 @@
+package uit.tool.app;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.*;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sb-app.fxml")));
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("App.fxml")));
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        String version_check = String.format("Hello, JavaFX %s, running on Java %s.", javafxVersion, javaVersion);
+        System.out.println(version_check);
+
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        primaryStage.setTitle("GVST");
+        primaryStage.setScene(new Scene(root, 1080, 720));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
