200f592f65e97571babf9971672a4ff2076d98ea
Lưu Hoàng Sơn <sonlh.cs.uit@gmail.com>
2021-07-04 14:14:27 +0700

    update menu document

--- app/src/main/java/uit/tool/app/components/event/AlgorithmEvent.java
+++ app/src/main/java/uit/tool/app/components/event/AlgorithmEvent.java
@@ -6,8 +6,7 @@ import javafx.event.EventType;
 public class AlgorithmEvent extends Event {
 	public static final EventType<AlgorithmEvent> DFS = new EventType<>(ANY,"ANIMATION_DFS");
 	public static final EventType<AlgorithmEvent> BFS = new EventType<>(ANY,"ANIMATION_BFS");
-	public static final EventType<AlgorithmEvent> DIJKSTRA = new EventType<>(ANY,"");
-	public static final EventType<AlgorithmEvent> GREEDY = new EventType<>(ANY,"GREEDY");
+	public static final EventType<AlgorithmEvent> DIJKSTRA = new EventType<>(ANY,"DIJKSTRA");
 	public static final EventType<AlgorithmEvent> HAM_PATH = new EventType<>(ANY,"HAMILTONIAN_PATH");
 	public static final EventType<AlgorithmEvent> HAM_CYCLE = new EventType<>(ANY,"HAMILTONIAN_CYCLE");
 	public static final EventType<AlgorithmEvent> EULER_PATH = new EventType<>(ANY,"EULERIAN_PATH");
--- app/src/main/java/uit/tool/app/components/menuComponent/Menu.java
+++ app/src/main/java/uit/tool/app/components/menuComponent/Menu.java
@@ -49,6 +49,7 @@ public class Menu extends VBox implements Loader {
 		this.nameTextField.textProperty().addListener((obs, oldV, newV) -> {
 			this.setting.setName(newV);
 		});
+
 		this.saveButton.setOnMouseClicked((MouseEvent event) -> {
 			this.fireEvent(new UserEvent(UserEvent.SAVE_GRAPH));
 		});
@@ -83,9 +84,12 @@ public class Menu extends VBox implements Loader {
 		return setting;
 	}
 
+	/**
+	 * Event handler tracks which button user clicked and emits new algorithm event
+	 * @param event The click event itself
+	 */
 	public void emitter(MouseEvent event) {
 		Object source = event.getSource();
-
 		if (GT_DFS.equals(source)) {
 			this.fireEvent(new AlgorithmEvent(AlgorithmEvent.DFS));
 		} else if (GT_BFS.equals(source)) {
