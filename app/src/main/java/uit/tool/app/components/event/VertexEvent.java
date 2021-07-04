756cdfa6305385de7558febf572fcff6b9330d56
Lưu Hoàng Sơn <sonlh.cs.uit@gmail.com>
2021-07-04 14:44:35 +0700

    update document in navigation & event

--- app/src/main/java/uit/tool/app/components/event/VertexEvent.java
+++ app/src/main/java/uit/tool/app/components/event/VertexEvent.java
@@ -5,39 +5,36 @@ import javafx.event.EventType;
 import uit.tool.app.components.visualizerComponent.graphComponent.VertexView;
 
 public class VertexEvent extends Event {
-	/**
-	 *
-	 */
+
 	public static final EventType<VertexEvent> MOVE = new EventType<>(ANY, "MOVE");
 	public static final EventType<VertexEvent> ADD = new EventType<>(ANY, "ADD");
 	public static final EventType<VertexEvent> RENAME = new EventType<>(ANY, "RENAME");
 	public static final EventType<VertexEvent> REMOVE = new EventType<>(ANY, "REMOVE");
 
-	private VertexView vertexView;
+	private final VertexView vertexView;
 	private double relativeX;
 	private double relativeY;
 
-
+	/**
+	 * An event was trigger when a user want to rename, remove or add an existed vertex
+	 */
 	public VertexEvent(EventType<? extends Event> type, VertexView vw) {
-		/**
-		 * An event was trigger when a user want to rename, remove or add an existed vertex
-		 */
 		super(type);
 		this.vertexView = vw;
 	}
 
+	/**
+	 * An event was trigger when a vertex moved. Remember that because of graph view need to resize so the
+	 * actual size bigger than viewport size, so when drag & drop, position of the cursor just relative
+	 * (with top & left edges of viewport, not actual pane)
+	 *
+	 * @param type the type of event
+	 * @param relativeX the distance from left edge of graph view to the cursor (dropped). Must provided if type is MOVE
+	 * @param relativeY the distance from top edge of graph view to the cursor (dropped). Must provided if type is MOVE
+	 *
+	 * @see javafx.scene.control.ScrollPane
+	 */
 	public VertexEvent(EventType<? extends Event> type, VertexView vw, double relativeX, double relativeY) {
-		/**
-		 * An event was trigger when a vertex moved. Remember that because of graph view need to resize so the
-		 * actual size bigger than viewport size, so when drag & drop, position of the cursor just relative
-		 * (with top & left edges of viewport, not actual pane)
-		 *
-		 * @param relativeX the distance from left edge of graph view to the cursor (dropped)
-		 * @param relativeY the distance from top edge of graph view to the cursor (dropped)
-		 *
-		 * @see javafx.scene.control.ScrollPane
-		 */
-
 		this(type, vw);
 		this.relativeX = relativeX;
 		this.relativeY = relativeY;
